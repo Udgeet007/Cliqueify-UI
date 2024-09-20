c1094b991d077cf50a87535024e9ba0adf8318dc
Udgeet <bhatt8udgeet@gmail.com>
2024-09-21 03:22:52 +0530

    Register Page Done.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
-import Login from "./pages/login/Login";
+// import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
-  return <Login />;
+  return <Register />;
 }
 
 export default App;
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -3,7 +3,29 @@ import './register.scss'
 const Register = () => {
   return (
     <div className="register">
-      Register
+       <div className="card">
+        <div className="left">
+          <h1>Lama Social.</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt
+            modi praesentium explicabo doloribus corporis amet, dignissimos enim
+            dolor officia quae laborum adipisci labore odio ducimus ratione
+            tempora, itaque fuga quasi!
+          </p>
+          <span>Dont you have an account.</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
